2e8911b388a1fadb19d88d4f8af8e17c1db0f044
PROTOTYPE9111 <prototype9111@list.ru>
2025-05-02 21:44:26 +0300

    Управление клавишами

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,10 @@ int main(){
                 window.close();
             }
         }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) { herosprite.move ({-0.1,0}); }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) { herosprite.move ({0.1,0}); }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) { herosprite.move ({0,-0.1}); }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) { herosprite.move ({0,0.1}); }
+        if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)))) { herosprite.move({-0.1, 0}); herosprite.setTextureRect(sf::IntRect({0, 96}, {96, 96})); } //добавили управление на клавиши W,S,A,D
+        if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)))) { herosprite.move({0.1, 0}); herosprite.setTextureRect(sf::IntRect({0, 192}, {96, 96})); } 
+        if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)))) { herosprite.move({0, -0.1}); herosprite.setTextureRect(sf::IntRect({0, 288}, {96, 96})); } 
+        if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)))) { herosprite.move({0, 0.1}); herosprite.setTextureRect(sf::IntRect({0, 0}, {96, 96})); }  
         window.clear();
         window.draw(herosprite);
         window.display();
