ac8f9886d5bb52e6aa5162a3a7bd77f4b7e1fc55
PROTOTYPE9111 <prototype9111@list.ru>
2025-05-02 21:12:33 +0300

    modified:   .vscode/c_cpp_properties.json 	new file:   src/.DS_Store 	new file:   src/images/wolf.png 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
-    sf::RenderWindow window(sf::VideoMode({500,500}), "Robotron test");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Blue);
+    sf::RenderWindow window(sf::VideoMode({800,650}), "Robotron test");
+    
+    sf::Image heroimage;
+    heroimage.loadFromFile("src/images/wolf.png");
+
+    sf::Texture herotexture;
+    herotexture.loadFromImage(heroimage);
+    
+    sf::Sprite herosprite(herotexture);
+    herosprite.setTextureRect(sf::IntRect({0, 192}, {96, 96}));
+
+    herosprite.setPosition ({50,25});
 
     while (window.isOpen()){
         while (auto event = window.pollEvent()) {
@@ -11,8 +20,12 @@ int main(){
                 window.close();
             }
         }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) { herosprite.move ({-0.1,0}); }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) { herosprite.move ({0.1,0}); }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) { herosprite.move ({0,-0.1}); }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) { herosprite.move ({0,0.1}); }
         window.clear();
-        window.draw(shape);
+        window.draw(herosprite);
         window.display();
     }
     return 0;
